2ebc79de1f192400e9de1ced0fe9f3d029574e8c
furbahadirkan <furbahadirkan@users.noreply.github.com>
2018-12-11 09:11:48 +0300

    Update sin.m

--- raspberrypidenemeleri/MATLAB/sin.m
+++ raspberrypidenemeleri/MATLAB/sin.m
@@ -1,26 +1,11 @@
-%64 sample
-clear
-sampling_rate=44100;
-frame_length=128;
-time=0:1/sampling_rate:1-1/sampling_rate;
+Fs=44100;
+time=0:1/Fs:1-1/Fs; %Create 0.5sn time vector
 
-channel_frequencies=[32,64,125,250,500,1e3,2e3,4e3,8e3,16e3];
+freq=2e3; % frequency of the sin
 
-for i=1:10
-   sines(i,:)=round(sin(2*pi*channel_frequencies(i).*time),2,'significant'); 
-    
-    hold on
-    plot(time,sines(i,:));
-    
-    
-end
-    time_2=0:1/44100:4-1/44100;
-    time_3=0:1/44100:1-1/44100;
-    ssss=sin(2*pi*e3.*time_3);
-    vvvv(:,1)=sin(2*pi*4e3.*time_3);
-    signal=[ssss,vvvv];
-    
-    audiowrite('sin2k-4k.wav',signal,44100);
-    
-    time=0:1/44100:1-1/44100;
-    
+sin2k(1,:)=round(sin(2*pi*freq.*time),2);
+
+csvwrite('sin2k.csv',sin2k);
+ audiowrite('sin1k.wav',sin1k,44100);
+ 
+ 
