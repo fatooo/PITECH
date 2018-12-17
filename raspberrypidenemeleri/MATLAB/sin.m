e53fc74137cb749e93e7b01249b73e4db0282367
furbahadirkan <furbahadirkan@users.noreply.github.com>
2018-12-18 00:31:53 +0300

    sad

--- raspberrypidenemeleri/MATLAB/sin.m
+++ raspberrypidenemeleri/MATLAB/sin.m
@@ -1,11 +1,11 @@
 Fs=44100;
-time=0:1/Fs:1-1/Fs; %Create 0.5sn time vector
+time=0:1/Fs:2-1/Fs; %Create 0.5sn time vector
 
-freq=6e3; % frequency of the sin
+freq=1e3; % frequency of the sin
 
-sin6k(1,:)=round(sin(2*pi*freq.*time),2);
+sin1k(1,:)=round(sin(2*pi*freq.*time),2);
 
-csvwrite('sin6k.csv',sin6k);
+csvwrite('sin1k.csv',sin1k);
  audiowrite('sin1k.wav',sin1k,44100);
  
  
