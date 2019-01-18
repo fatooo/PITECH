dde0581b06cfd4f17400f2335718b9bec46c58cf
Aycan <abeyenir@gmail.com>
2019-01-18 21:49:41 +0300

    3

---
+++ Mikrop/447-Final Project/Assembly/Aycan Denemeler/ADDRESS_CHANGE.s
@@ -0,0 +1,71 @@
+;*************************************************************** 
+; EQU Directives
+; These directives do not allocate memory
+;***************************************************************
+;SYMBOL				DIRECTIVE	VALUE			COMMENT
+OUT_PORTB_DC		EQU			0x40005008		;00000010
+
+SSI0_DR				EQU			0x40008008
+SSI0_SR				EQU			0x4000800C
+;***************************************************************
+; Directives - This Data Section is part of the code
+; It is in the read only section  so values cannot be changed.
+;***************************************************************	
+;LABEL		DIRECTIVE	VALUE					COMMENT
+            AREA       	sdata, DATA, READONLY
+            THUMB
+		
+;***************************************************************
+; Program section					      
+;***************************************************************
+;LABEL		DIRECTIVE	VALUE					COMMENT
+			AREA		routines,READONLY,CODE
+			THUMB
+			ALIGN
+			EXTERN		DELAY_100ms
+			EXPORT		ADDRESS_CHANGE
+
+;***************************************************************
+;	Main Function
+;	Takes Coordinates in R4 and sends them to screen
+;	Least Significant byte is X and Second one is Y
+;***************************************************************	
+;LABEL		DIRECTIVE	VALUE					COMMENT
+
+ADDRESS_CHANGE	PROC
+			
+			PUSH		{LR}
+			
+			LDR			R5,=OUT_PORTB_DC
+			MOV			R1,#0x00
+			STR			R1,[R5]
+			
+			AND			R1,R4,#0xFF			;X coordinate
+			AND			R2,R4,#0xFF00		;Y coordinates
+			LSR			R2,R2,#8			;shift 1 byte Right
+			
+			
+			
+control		LDR			R5,=SSI0_SR
+			LDR			R0,[R5]
+			AND			R0,#0x02
+			CMP			R0,#0x02
+			BNE			control
+			
+			
+			LDR			R5,=SSI0_DR				
+			STR			R1,[R5]				;Send X coordinate
+			NOP
+			NOP
+			NOP
+			STR			R2,[R5]				;send Y coordinate
+			
+			POP			{LR}
+			BX			LR
+;***************************************************************
+; End of the program  section
+;***************************************************************
+;LABEL      DIRECTIVE       VALUE                           COMMENT
+			ENDP
+			ALIGN
+			END	
