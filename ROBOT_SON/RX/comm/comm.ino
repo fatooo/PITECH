e52201d85a875ac194902ec7b17189bf96aa2be7
furbahadirkan <furbahadirkan@users.noreply.github.com>
2019-04-04 10:55:11 +0300

    asd

---
+++ ROBOT_SON/RX/comm/comm.ino
@@ -0,0 +1,143 @@
+#include <SPI.h>
+#include <Servo.h> //Servo kütüphanesini ekledik.
+#include "nRF24L01.h"
+#include "RF24.h"  
+
+
+#define enA 22
+#define enB 23
+#define in1 24
+#define in2 26
+#define in3 28
+#define in4 30
+#define in5 32
+#define in6 34
+
+
+
+//Modül ile ilgili kütüphaneleri ekliyoruz
+int mesaj[1];
+RF24 alici(9,53); //CE,CSN
+const uint64_t kanal = 0xE8E8F0F0E1LL;
+bool sag = false;
+bool sol = false;
+bool ileri = false;
+bool geri = false;
+bool shoot = false;
+String karar ;
+
+
+
+
+void setup(void){
+ Serial.begin(9600);
+ 
+ alici.begin();
+ alici.openReadingPipe(1,kanal);
+ alici.startListening();
+
+
+
+  pinMode(enA, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+   pinMode(in5, OUTPUT);
+  pinMode(in6, OUTPUT);
+
+
+ }
+
+void loop(void){
+ if (alici.available())
+ {
+   bool done = false;    
+   
+     done = alici.read(mesaj, 1);   
+
+        if(mesaj[0]==16){
+          
+          karar = "shoot";
+
+           digitalWrite(in1, LOW);
+            digitalWrite(in2, LOW);
+             digitalWrite(in3, LOW);
+             digitalWrite(in4, LOW);
+            digitalWrite(in5, HIGH);
+             digitalWrite(in6, LOW);
+         
+                 
+          
+        }
+        if(mesaj[0]==8){
+          
+         karar =  "ileri";
+            digitalWrite(enA, HIGH);
+            digitalWrite(enB, HIGH);
+             digitalWrite(in1, LOW);
+             digitalWrite(in2, HIGH);
+             digitalWrite(in3, LOW);
+             digitalWrite(in4, HIGH);
+             digitalWrite(in5, LOW);
+             digitalWrite(in6, LOW);
+         
+        }
+        if (mesaj[0]==4){
+         karar = "geri";
+            digitalWrite(enA, HIGH);
+            digitalWrite(enB, HIGH);
+
+             digitalWrite(in1, HIGH);
+             digitalWrite(in2, LOW);
+             digitalWrite(in3, HIGH);
+             digitalWrite(in4, LOW);
+             digitalWrite(in5, LOW);
+             digitalWrite(in6, LOW);
+
+         
+        }
+        if(mesaj[0]==2){
+          karar="sag";
+                      digitalWrite(enA, HIGH);
+            digitalWrite(enB, HIGH);
+
+             digitalWrite(in1, LOW);
+             digitalWrite(in2, HIGH);
+             digitalWrite(in3, HIGH);
+             digitalWrite(in4, LOW);
+             digitalWrite(in5, LOW);
+             digitalWrite(in6, LOW);
+        }
+        if(mesaj[0]==1){
+          karar="sol";
+                      digitalWrite(enA, HIGH);
+            digitalWrite(enB, HIGH);
+             digitalWrite(in1, HIGH);
+             digitalWrite(in2, LOW);
+             digitalWrite(in3, LOW);
+             digitalWrite(in4, HIGH);
+             digitalWrite(in5, LOW);
+             digitalWrite(in6, LOW);
+        }
+        if(mesaj[0]==0){
+          karar= "dur";
+            digitalWrite(enA, HIGH);
+            digitalWrite(enB, HIGH);
+
+            digitalWrite(in1, LOW);
+            digitalWrite(in2, LOW);
+            digitalWrite(in3, LOW);
+            digitalWrite(in4, LOW);
+            digitalWrite(in5, LOW);
+            digitalWrite(in6, LOW);
+          
+        }
+       
+      
+     Serial.println(karar);
+   
+     delay(10);
+   
+ }
+ }
---
+++ ROBOT_SON/TX/tx.ino
@@ -0,0 +1,48 @@
+#include  <SPI.h> 
+#include "nRF24L01.h"
+#include "RF24.h"     //Modül ile ilgili kütüphaneleri ekliyoruz
+RF24 verici(9,10);     //kütüphane tarafından kullanılacak pinleri tanımlıyoruz (CE,CSN)
+const uint64_t kanal = 0xE8E8F0F0E1LL; //kanalı tanımlıyoruz  
+int joystick_x_pin = A0;
+ int joystick_y_pin = A1;
+ int joystick_switch_pin = A2;
+ int joystick_x=0;
+ int joystick_y=0;
+ int joystick_switch = 0;
+ int message[1] ; 
+void setup(void)
+{
+ verici.begin();       //nrf yi başlatıyoruz
+ verici.openWritingPipe(kanal);  //kanal id sı tanımlanıyor
+}
+void loop(void)
+{  
+     //Reading commands from joystick
+     joystick_x = analogRead(joystick_x_pin)-512;
+     joystick_y = analogRead(joystick_y_pin)-512;
+     joystick_switch = analogRead(joystick_switch_pin);
+       if(joystick_y>400){
+        
+            message[0]=B00000010;
+           // Serial.println(joystick_x);      
+          }
+        else if (joystick_y<-400){      
+            message[0]=B00000001;     
+             // Serial.println(joystick_x);
+              }
+       else if (joystick_x>400){
+                message[0] = B00001000;
+               //Serial.println(joystick_y);      
+              }
+        else if (joystick_x<-400){
+                message[0]=B00000100;
+                //Serial.println(joystick_y);
+               }
+        else if  (joystick_switch==0) {
+               message[0]=B00010000;
+                }
+        else  {
+               message[0]=B00000000;
+              }  
+ verici.write(message, 1);   //mesaj değişkeni yollanıyor 
+ }
