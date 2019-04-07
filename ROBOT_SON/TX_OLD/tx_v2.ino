38e946eb369635c8db7bc24ff785ca3e04146a30
furbahadirkan <furbahadirkan@users.noreply.github.com>
2019-04-07 18:49:31 +0300

    txxx

--- ROBOT_SON/TX_OLD/tx_v2.ino
+++ ROBOT_SON/TX_OLD/tx_v2.ino
@@ -21,50 +21,28 @@ void loop(void)
      joystick_x = analogRead(joystick_x_pin)-512;
      joystick_y = analogRead(joystick_y_pin)-512;
      joystick_switch = analogRead(joystick_switch_pin);
-       if(joystick_y>450){
+       if(joystick_y>400){
         
-            message[0]=2;
+            message[0]=B00000010;
            // Serial.println(joystick_x);      
           }
-        else if (joystick_y<-450){      
-            message[0]=1;     
+        else if (joystick_y<-400){      
+            message[0]=B00000001;     
              // Serial.println(joystick_x);
               }
-       else if (joystick_x>450){
-                message[0] = 8;
+       else if (joystick_x>400){
+                message[0] = B00001000;
                //Serial.println(joystick_y);      
               }
-        else if (joystick_x<-450){
-                message[0]=4;
+        else if (joystick_x<-400){
+                message[0]=B00000100;
                 //Serial.println(joystick_y);
                }
         else if  (joystick_switch==0) {
-               message[0]=16;
+               message[0]=B00010000;
                 }
-
-        else if (joystick_y<450 && joystick_y>100) {
-                message[0]=32; 
-          
-          }
-          else if (joystick_y<-100 && joystick_y>-450) {
-                message[0]=64; 
-          
-          }
-
-           else if (joystick_x>100 && joystick_x<450){
-                message[0] = 9;
-                    
-              }
-        else if (joystick_x<-100 && joystick_x>-450){
-                message[0]=5;
-                //Serial.println(joystick_y);
-               }        
-
-
-                
         else  {
                message[0]=B00000000;
-              } 
-              delay(1); 
+              }  
  verici.write(message, 1);   //mesaj değişkeni yollanıyor 
  }
